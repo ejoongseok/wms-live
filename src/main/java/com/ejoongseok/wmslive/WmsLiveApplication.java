65743918232a80903d8cbae6f03026a8a7b1882f
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-30 15:33:47 +0900

    Initial commit

---
+++ src/main/java/com/ejoongseok/wmslive/WmsLiveApplication.java
@@ -0,0 +1,13 @@
+package com.ejoongseok.wmslive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ejoongseok/wmslive/WmsLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejoongseok.wmslive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
