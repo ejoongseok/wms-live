e6aaa62944547d2980996bd4f8f4874edd6ffbd7
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-30 15:37:23 +0900

    2.create test case registerProduct

--- src/test/java/com/ejoongseok/wmslive/product/feature/RegisterProductTest.java
+++ src/test/java/com/ejoongseok/wmslive/product/feature/RegisterProductTest.java
@@ -1,4 +1,14 @@
 package com.ejoongseok.wmslive.product.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterProductTest {
+
+    @Test
+    @DisplayName("상품을 등록한다.")
+    void registerProduct() {
+
+
+    }
 }
