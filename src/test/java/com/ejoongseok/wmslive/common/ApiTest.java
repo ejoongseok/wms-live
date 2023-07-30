b83fca6f6cb9578ab7c3962ec983d5668dd229ba
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-30 16:40:39 +0900

    14.database cleaner 추가

--- src/test/java/com/ejoongseok/wmslive/common/ApiTest.java
+++ src/test/java/com/ejoongseok/wmslive/common/ApiTest.java
@@ -2,6 +2,7 @@ package com.ejoongseok.wmslive.common;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -9,11 +10,15 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 public class ApiTest {
     @LocalServerPort
     private int port;
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
 
     @BeforeEach
     void setUp() {
         if (RestAssured.UNDEFINED_PORT == RestAssured.port) {
             RestAssured.port = port;
+            databaseCleaner.afterPropertiesSet();
         }
+        databaseCleaner.execute();
     }
 }
