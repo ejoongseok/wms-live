cfa705d02a9d8d98db3a7c4ae7348a5e64a031c2
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-30 16:19:23 +0900

    10.create RegisterProductApi.

--- src/test/java/com/ejoongseok/wmslive/product/feature/RegisterProductTest.java
+++ src/test/java/com/ejoongseok/wmslive/product/feature/RegisterProductTest.java
@@ -1,15 +1,11 @@
 package com.ejoongseok.wmslive.product.feature;
 
 import com.ejoongseok.wmslive.common.ApiTest;
-import com.ejoongseok.wmslive.product.domain.Category;
 import com.ejoongseok.wmslive.product.domain.ProductRepository;
-import com.ejoongseok.wmslive.product.domain.TemperatureZone;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import com.ejoongseok.wmslive.product.feature.api.RegisterProductApi;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,43 +16,8 @@ class RegisterProductTest extends ApiTest {
     @Test
     @DisplayName("상품을 등록한다.")
     void registerProduct() {
-        //given
-        final String name = "name";
-        final String code = "code";
-        final String description = "description";
-        final String brand = "brand";
-        final String maker = "maker";
-        final String origin = "origin";
-        final Category category = Category.ELECTRONICS;
-        final TemperatureZone temperatureZone = TemperatureZone.ROOM_TEMPERATURE;
-        final Long weightInGrams = 1000L;
-        final Long widthInMillimeters = 100L;
-        final Long heightInMillimeters = 100L;
-        final Long lengthInMillimeters = 100L;
-        final RegisterProduct.Request request = new RegisterProduct.Request(
-                name,
-                code,
-                description,
-                brand,
-                maker,
-                origin,
-                category,
-                temperatureZone,
-                weightInGrams, // gram
-                widthInMillimeters, // 너비 mm
-                heightInMillimeters, // 높이 mm
-                lengthInMillimeters // 길이 mm
-        );
-        //when
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(request)
-                .when()
-                .post("/products")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+        new RegisterProductApi().request();
 
-        //then
         assertThat(productRepository.findAll()).hasSize(1);
     }
 
