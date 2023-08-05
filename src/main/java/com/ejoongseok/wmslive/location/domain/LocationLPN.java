efc4ea76bf335158d9346b1461313edd9ceed316
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:48:45 +0900

    21.create fields LocationLPN.

--- src/main/java/com/ejoongseok/wmslive/location/domain/LocationLPN.java
+++ src/main/java/com/ejoongseok/wmslive/location/domain/LocationLPN.java
@@ -1,4 +1,23 @@
 package com.ejoongseok.wmslive.location.domain;
 
+import com.ejoongseok.wmslive.inbound.domain.LPN;
+import lombok.Getter;
+
 public class LocationLPN {
+
+    private final Location location;
+    @Getter
+    private final LPN lpn;
+    private Long inventoryQuantity;
+
+    public LocationLPN(final Location location, final LPN lpn) {
+        this.location = location;
+        this.lpn = lpn;
+        inventoryQuantity = 1L;
+    }
+
+    public void increaseQuantity() {
+        inventoryQuantity++;
+    }
+
 }
