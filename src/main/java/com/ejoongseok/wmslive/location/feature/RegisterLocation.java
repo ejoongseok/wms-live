b8a2ea663d654b85064483d370e82a425e02ccfa
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:05:23 +0900

    18.change request validation: springframework.Assert -> jakarta.validation.

--- src/main/java/com/ejoongseok/wmslive/location/feature/RegisterLocation.java
+++ src/main/java/com/ejoongseok/wmslive/location/feature/RegisterLocation.java
@@ -4,9 +4,11 @@ import com.ejoongseok.wmslive.location.domain.Location;
 import com.ejoongseok.wmslive.location.domain.LocationRepository;
 import com.ejoongseok.wmslive.location.domain.StorageType;
 import com.ejoongseok.wmslive.location.domain.UsagePurpose;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,22 +21,19 @@ public class RegisterLocation {
 
     @PostMapping("/locations")
     @ResponseStatus(HttpStatus.CREATED)
-    public void request(@RequestBody final Request request) {
+    public void request(@RequestBody @Valid final Request request) {
         final Location location = request.toDomain();
 
         locationRepository.save(location);
     }
 
     public record Request(
+            @NotBlank(message = "로케이션 바코드는 필수입니다.")
             String locationBarcode,
+            @NotNull(message = "보관 타입은 필수입니다.")
             StorageType storageType,
+            @NotNull(message = "보관 목적은 필수입니다.")
             UsagePurpose usagePurpose) {
-
-        public Request {
-            Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
-            Assert.notNull(storageType, "보관 타입은 필수입니다.");
-            Assert.notNull(usagePurpose, "보관 목적은 필수입니다.");
-        }
 
         public Location toDomain() {
             return new Location(
