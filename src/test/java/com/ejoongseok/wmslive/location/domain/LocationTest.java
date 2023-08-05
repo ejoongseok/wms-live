ea8002efeb505fb7baedfb48b7489ef201390d61
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:36:29 +0900

    17.extract method.

--- src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
@@ -13,18 +13,26 @@ class LocationTest {
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLPN() {
-        final String locationBarcode = "A-1-1";
-        final StorageType storageType = StorageType.TOTE;
-        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
-        final Location location = new Location(locationBarcode, storageType, usagePurpose);
+        final Location location = createLocation();
 
 
-        final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
-        final String lpnBarcode = "LPN-1";
-        final LPN lpn = new LPN(lpnBarcode, expirationAt, anInboundItem().build());
+        final LPN lpn = createLPN();
 
         location.assignLPN(lpn);
 
         //TODO locaiton에 locationLPN 목록이 비어있지 않은지 확인한다. 추가된 로케이션 LPN의 재고가 1이어야합니다.
+    }
+
+    private LPN createLPN() {
+        final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
+        final String lpnBarcode = "LPN-1";
+        return new LPN(lpnBarcode, expirationAt, anInboundItem().build());
+    }
+
+    private Location createLocation() {
+        final String locationBarcode = "A-1-1";
+        final StorageType storageType = StorageType.TOTE;
+        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
+        return new Location(locationBarcode, storageType, usagePurpose);
     }
 }
