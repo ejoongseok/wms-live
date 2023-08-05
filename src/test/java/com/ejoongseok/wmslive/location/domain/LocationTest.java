51ee3fecd7445762eae27888410b2485d384348b
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:54:39 +0900

    27.테스트 보완.

--- src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
@@ -26,10 +26,11 @@ class LocationTest {
     @DisplayName("로케이션에 LPN을 할당한다. 이미 LPN이 존재하면 생성하지 않고 재고만 증가시킨다.")
     void already_exsits_assignLPN() {
         final Location location = aLocation().build();
-        final LPN lpn = anLPN().build();
+        final LPN lpn = anLPN().lpnBarcode("LPNBARCODE").build();
+        final LPN lpn2 = anLPN().lpnBarcode("LPNBARCODE").build();
 
         location.assignLPN(lpn);
-        location.assignLPN(lpn);
+        location.assignLPN(lpn2);
 
         assertThat(location.getLocationLPNList()).hasSize(1);
         assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(2L);
