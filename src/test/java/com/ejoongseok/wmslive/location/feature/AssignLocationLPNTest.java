df7671ecd17841247a3c487a93db12549b7fb321
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:18:00 +0900

    2.create test case assignLocationLPN.

--- src/test/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPNTest.java
@@ -1,5 +1,13 @@
 package com.ejoongseok.wmslive.location.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class AssignLocationLPNTest {
 
+    @Test
+    @DisplayName("로케이션에 LPN을 할당한다.")
+    void assignLocationLPN() {
+
+    }
 }
