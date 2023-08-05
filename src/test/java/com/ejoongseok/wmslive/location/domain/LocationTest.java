91cc753303b900f31e88b25bf0ad130d1162b9c4
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:42:42 +0900

    19.extract delegate: LocationFixture.

--- src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
@@ -5,26 +5,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.ejoongseok.wmslive.inbound.domain.LPNFixture.anLPN;
+import static com.ejoongseok.wmslive.location.domain.LocationFixture.aLocation;
 
 class LocationTest {
-
 
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLPN() {
-        final Location location = createLocation();
-
+        final Location location = aLocation().build();
         final LPN lpn = anLPN().build();
 
         location.assignLPN(lpn);
 
         //TODO locaiton에 locationLPN 목록이 비어있지 않은지 확인한다. 추가된 로케이션 LPN의 재고가 1이어야합니다.
-    }
-
-    private Location createLocation() {
-        final String locationBarcode = "A-1-1";
-        final StorageType storageType = StorageType.TOTE;
-        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
-        return new Location(locationBarcode, storageType, usagePurpose);
     }
 }
