324b68694b7ae1c12b4608f3857d1134350b17e6
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 16:42:07 +0900

    3.create inner class RegisterLocation.

--- src/test/java/com/ejoongseok/wmslive/location/fature/RegisterLocationTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/fature/RegisterLocationTest.java
@@ -1,14 +1,25 @@
 package com.ejoongseok.wmslive.location.fature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterLocationTest {
 
 
+    private RegisterLocation registerLocation;
+
+    @BeforeEach
+    void setUp() {
+        registerLocation = new RegisterLocation();
+    }
+
     @Test
     @DisplayName("로케이션을 등록한다.")
     void registerLocation() {
 
+    }
+
+    private class RegisterLocation {
     }
 }
