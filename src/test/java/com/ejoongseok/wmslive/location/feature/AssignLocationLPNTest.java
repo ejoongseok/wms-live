0e9ee71df6a09c32f8e22c965b13eb5d1fe1f194
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:18:46 +0900

    4.create method: request.

--- src/test/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPNTest.java
@@ -16,9 +16,12 @@ class AssignLocationLPNTest {
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLocationLPN() {
-
+        assignLocationLPN.request();
     }
 
     private class AssignLocationLPN {
+        public void request() {
+
+        }
     }
 }
