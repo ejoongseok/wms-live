0e0a28a26df14bfd35702ac345d0486ff9076a73
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 18:01:16 +0900

    29.extract method.

--- src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
@@ -4,6 +4,8 @@ import com.ejoongseok.wmslive.inbound.domain.LPN;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static com.ejoongseok.wmslive.inbound.domain.LPNFixture.anLPN;
 import static com.ejoongseok.wmslive.location.domain.LocationFixture.aLocation;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,21 +20,28 @@ class LocationTest {
 
         location.assignLPN(lpn);
 
-        assertThat(location.getLocationLPNList()).hasSize(1);
-        assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(1L);
+        assertAssignLPN(location, 1L);
+    }
+
+    private void assertAssignLPN(
+            final Location location,
+            final Long expectedInventoryQuantity) {
+        final List<LocationLPN> locationLPNList = location.getLocationLPNList();
+        final LocationLPN locationLPN = locationLPNList.get(0);
+        assertThat(locationLPNList).hasSize(1);
+        assertThat(locationLPN.getInventoryQuantity()).isEqualTo(expectedInventoryQuantity);
     }
 
     @Test
     @DisplayName("로케이션에 LPN을 할당한다. 이미 LPN이 존재하면 생성하지 않고 재고만 증가시킨다.")
     void already_exsits_assignLPN() {
         final Location location = aLocation().build();
-        final LPN lpn = anLPN().lpnBarcode("LPNBARCODE").build();
-        final LPN lpn2 = anLPN().lpnBarcode("LPNBARCODE").build();
+        final LPN lpn = anLPN().build();
+        final LPN lpn2 = anLPN().build();
 
         location.assignLPN(lpn);
         location.assignLPN(lpn2);
 
-        assertThat(location.getLocationLPNList()).hasSize(1);
-        assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(2L);
+        assertAssignLPN(location, 2L);
     }
 }
