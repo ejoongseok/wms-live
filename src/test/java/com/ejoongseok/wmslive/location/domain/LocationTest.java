1e9d317466136121b45e1fe73988a7c778eec516
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:34:00 +0900

    15.create test class: LocationTest.

---
+++ src/test/java/com/ejoongseok/wmslive/location/domain/LocationTest.java
@@ -0,0 +1,12 @@
+package com.ejoongseok.wmslive.location.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LocationTest {
+
+    @Test
+    @DisplayName("로케이션에 LPN을 할당한다.")
+    void assignLPN() {
+    }
+}
