3fed1ab0111ece85e2726b40fdd0bae1c118ca5d
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 16:41:19 +0900

    2.create test case RegisterLocation.

--- src/test/java/com/ejoongseok/wmslive/location/fature/RegisterLocationTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/fature/RegisterLocationTest.java
@@ -1,5 +1,14 @@
 package com.ejoongseok.wmslive.location.fature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterLocationTest {
 
+
+    @Test
+    @DisplayName("로케이션을 등록한다.")
+    void registerLocation() {
+
+    }
 }
