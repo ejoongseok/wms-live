7d809e817317c60238cbcbaa5ea6556abafbc4b8
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 18:24:53 +0900

    40.change validation: springframework.assert -> jakarta.validation.

--- src/main/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPN.java
+++ src/main/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPN.java
@@ -4,9 +4,10 @@ import com.ejoongseok.wmslive.inbound.domain.LPN;
 import com.ejoongseok.wmslive.inbound.domain.LPNRepository;
 import com.ejoongseok.wmslive.location.domain.Location;
 import com.ejoongseok.wmslive.location.domain.LocationRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class AssignLocationLPN {
 
     @Transactional
     @PostMapping("/locations/assign-lpn")
-    public void request(@RequestBody final Request request) {
+    public void request(@RequestBody @Valid final Request request) {
         final Location location = locationRepository.getByLocationBarcode(request.locationBarcode);
         final LPN lpn = lpnRepository.getByLPNBarcode(request.lpnBarcode);
 
@@ -27,12 +28,9 @@ public class AssignLocationLPN {
     }
 
     public record Request(
+            @NotBlank(message = "로케이션 바코드는 필수입니다.")
             String locationBarcode,
+            @NotBlank(message = "LPN 바코드는 필수입니다.")
             String lpnBarcode) {
-
-        public Request {
-            Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
-            Assert.hasText(lpnBarcode, "LPN 바코드는 필수입니다.");
-        }
     }
 }
