d49ca2aac356fb83f9989bae540428efe7f5d27c
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-05 17:25:36 +0900

    8.create method: findByLocationBarcode.

--- src/main/java/com/ejoongseok/wmslive/location/domain/LocationRepository.java
+++ src/main/java/com/ejoongseok/wmslive/location/domain/LocationRepository.java
@@ -1,6 +1,11 @@
 package com.ejoongseok.wmslive.location.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    @Query("select l from Location l where l.locationBarcode = :locationBarcode")
+    Optional<Location> findByLocationBarcode(String locationBarcode);
 }
--- src/test/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/ejoongseok/wmslive/location/feature/AssignLocationLPNTest.java
@@ -1,5 +1,7 @@
 package com.ejoongseok.wmslive.location.feature;
 
+import com.ejoongseok.wmslive.location.domain.Location;
+import com.ejoongseok.wmslive.location.domain.LocationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,18 +19,24 @@ class AssignLocationLPNTest {
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLocationLPN() {
+        //given
         final String locationBarcode = "A-1-1";
         final String lpnBarcode = "LPN-1";
         final AssignLocationLPN.Request request = new AssignLocationLPN.Request(
                 locationBarcode,
                 lpnBarcode
         );
+        //when
         assignLocationLPN.request(request);
+        //then
+        //TODO locaiton에 locationLPN 목록이 비어있지 않은지 확인한다.
     }
 
     private class AssignLocationLPN {
-        public void request(final Request request) {
+        private LocationRepository locationRepository;
 
+        public void request(final Request request) {
+            final Location location = locationRepository.findByLocationBarcode(request.locationBarcode).get();
         }
 
         public record Request(
