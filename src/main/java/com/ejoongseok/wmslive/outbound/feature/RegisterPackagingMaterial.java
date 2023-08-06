6a8651ad6fa4eeadd4594a09f4b81397bcf0c65e
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-06 17:09:45 +0900

    15.change request validate: spring.assert -> jakarta.validation

--- src/main/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterial.java
+++ src/main/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterial.java
@@ -4,9 +4,12 @@ import com.ejoongseok.wmslive.outbound.domain.MaterialType;
 import com.ejoongseok.wmslive.outbound.domain.PackagingMaterial;
 import com.ejoongseok.wmslive.outbound.domain.PackagingMaterialDimension;
 import com.ejoongseok.wmslive.outbound.domain.PackagingMaterialRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,61 +23,43 @@ class RegisterPackagingMaterial {
 
     @PostMapping("/packaging-materials")
     @ResponseStatus(HttpStatus.CREATED)
-    public void request(@RequestBody final Request request) {
+    public void request(@RequestBody @Valid final Request request) {
         final PackagingMaterial packagingMaterial = request.toDomain();
 
         packagingMaterialRepository.save(packagingMaterial);
     }
 
     public record Request(
+            @NotBlank(message = "포장재 이름은 필수입니다.")
             String name,
+            @NotBlank(message = "포장재 코드는 필수입니다.")
             String code,
+            @NotNull(message = "내부 폭은 필수입니다.")
+            @Min(value = 1, message = "내부 폭은 1mm 이상이어야 합니다.")
             Long innerWidthInMillimeters,
+            @NotNull(message = "내부 높이는 필수입니다.")
+            @Min(value = 1, message = "내부 높이는 1mm 이상이어야 합니다.")
             Long innerHeightInMillimeters,
+            @NotNull(message = "내부 길이는 필수입니다.")
+            @Min(value = 1, message = "내부 길이는 1mm 이상이어야 합니다.")
             Long innerLengthInMillimeters,
+            @NotNull(message = "외부 폭은 필수입니다.")
+            @Min(value = 1, message = "외부 폭은 1mm 이상이어야 합니다.")
             Long outerWidthInMillimeters,
+            @NotNull(message = "외부 높이는 필수입니다.")
+            @Min(value = 1, message = "외부 높이는 1mm 이상이어야 합니다.")
             Long outerHeightInMillimeters,
+            @NotNull(message = "외부 길이는 필수입니다.")
+            @Min(value = 1, message = "외부 길이는 1mm 이상이어야 합니다.")
             Long outerLengthInMillimeters,
+            @NotNull(message = "무게는 필수입니다.")
+            @Min(value = 1, message = "무게는 1g 이상이어야 합니다.")
             Long weightInGrams,
+            @NotNull(message = "최대 무게는 필수입니다.")
+            @Min(value = 1, message = "최대 무게는 1g 이상이어야 합니다.")
             Long maxWeightInGrams,
+            @NotNull(message = "포장재 타입은 필수입니다.")
             MaterialType materialType) {
-        public Request {
-            Assert.hasText(name, "포장재 이름은 필수입니다.");
-            Assert.hasText(code, "포장재 코드는 필수입니다.");
-            Assert.notNull(innerWidthInMillimeters, "내부 폭은 필수입니다.");
-            if (1 > innerWidthInMillimeters) {
-                throw new IllegalArgumentException("내부 폭은 1mm 이상이어야 합니다.");
-            }
-            Assert.notNull(innerHeightInMillimeters, "내부 높이는 필수입니다.");
-            if (1 > innerHeightInMillimeters) {
-                throw new IllegalArgumentException("내부 높이는 1mm 이상이어야 합니다.");
-            }
-            Assert.notNull(innerLengthInMillimeters, "내부 길이는 필수입니다.");
-            if (1 > innerLengthInMillimeters) {
-                throw new IllegalArgumentException("내부 길이는 1mm 이상이어야 합니다.");
-            }
-            Assert.notNull(outerWidthInMillimeters, "외부 폭은 필수입니다.");
-            if (1 > outerWidthInMillimeters) {
-                throw new IllegalArgumentException("외부 폭은 1mm 이상이어야 합니다.");
-            }
-            Assert.notNull(outerHeightInMillimeters, "외부 높이는 필수입니다.");
-            if (1 > outerHeightInMillimeters) {
-                throw new IllegalArgumentException("외부 높이는 1mm 이상이어야 합니다.");
-            }
-            Assert.notNull(outerLengthInMillimeters, "외부 길이는 필수입니다.");
-            if (1 > outerLengthInMillimeters) {
-                throw new IllegalArgumentException("외부 길이는 1mm 이상이어야 합니다.");
-            }
-            Assert.notNull(weightInGrams, "무게는 필수입니다.");
-            if (1 > weightInGrams) {
-                throw new IllegalArgumentException("무게는 1g 이상이어야 합니다.");
-            }
-            Assert.notNull(maxWeightInGrams, "최대 무게는 필수입니다.");
-            if (1 > maxWeightInGrams) {
-                throw new IllegalArgumentException("최대 무게는 1g 이상이어야 합니다.");
-            }
-            Assert.notNull(materialType, "포장재 종류는 필수입니다.");
-        }
 
         public PackagingMaterial toDomain() {
             return new PackagingMaterial(
