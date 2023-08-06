3ce24288b7270cd889cf5e8eb0586fafffe7bfc0
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-06 16:52:44 +0900

    6.change signature.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
@@ -16,8 +16,38 @@ class RegisterPackagingMaterialTest {
     @Test
     @DisplayName("포장재를 등록한다.")
     void registerPackagingMaterial() {
-        final RegisterPackagingMaterial.Request request = new RegisterPackagingMaterial.Request();
+        final Long innerWidthInMillimeters = 1000L;
+        final Long innerHeightInMillimeters = 1000L;
+        final Long innerLengthInMillimeters = 1000L;
+        final Long outerWidthInMillimeters = 1000L;
+        final Long outerHeightInMillimeters = 1000L;
+        final Long outerLengthInMillimeters = 1000L;
+        final Long weightInGrams = 100L;
+        final Long maxWeightInGrams = 10000L;
+        final RegisterPackagingMaterial.Request request = new RegisterPackagingMaterial.Request(
+                "name",
+                "code",
+                innerWidthInMillimeters,
+                innerHeightInMillimeters,
+                innerLengthInMillimeters,
+                outerWidthInMillimeters,
+                outerHeightInMillimeters,
+                outerLengthInMillimeters,
+                weightInGrams,
+                maxWeightInGrams,
+                MaterialType.CORRUGATED_BOX
+        );
+
         registerPackagingMaterial.request(request);
+    }
+
+    enum MaterialType {
+        CORRUGATED_BOX("골판지 상자");
+        private final String description;
+
+        MaterialType(final String description) {
+            this.description = description;
+        }
     }
 
     private class RegisterPackagingMaterial {
@@ -25,7 +55,10 @@ class RegisterPackagingMaterialTest {
 
         }
 
-        public record Request() {
+        public record Request(String name, String code, Long innerWidthInMillimeters, Long innerHeightInMillimeters,
+                              Long innerLengthInMillimeters, Long outerWidthInMillimeters,
+                              Long outerHeightInMillimeters, Long outerLengthInMillimeters, Long weightInGrams,
+                              Long maxWeightInGrams, MaterialType corrugatedBox) {
         }
     }
 }
