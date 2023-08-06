dd9268866f5f8e99f13b22b853d8b94dd6b35751
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-06 16:48:21 +0900

    2.create test case.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
@@ -1,6 +1,13 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterPackagingMaterialTest {
 
+    @Test
+    @DisplayName("포장재를 등록한다.")
+    void registerPackagingMaterial() {
 
+    }
 }
