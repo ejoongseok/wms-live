ea5fc23468cd1a6e8de34a15e737d9d4f4e5e874
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-06 16:48:46 +0900

    3.create inner class: RegisterPackagingMaterial.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterPackagingMaterialTest.java
@@ -1,13 +1,23 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterPackagingMaterialTest {
 
+    private RegisterPackagingMaterial registerPackagingMaterial;
+
+    @BeforeEach
+    void setUp() {
+        registerPackagingMaterial = new RegisterPackagingMaterial();
+    }
+
     @Test
     @DisplayName("포장재를 등록한다.")
     void registerPackagingMaterial() {
+    }
 
+    private class RegisterPackagingMaterial {
     }
 }
