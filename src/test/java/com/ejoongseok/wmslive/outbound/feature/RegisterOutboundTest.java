081f2c939890a58af20535cd4021e51d8aa3dc9e
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-07 20:34:49 +0900

    6.change signature.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterOutboundTest.java
@@ -3,6 +3,9 @@ package com.ejoongseok.wmslive.outbound.feature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
 
 class RegisterOutboundTest {
 
@@ -16,7 +19,14 @@ class RegisterOutboundTest {
     @Test
     @DisplayName("출고를 등록한다.")
     void registerOutbound() {
-        final RegisterOutbound.Request request = new RegisterOutbound.Request();
+        final Long orderNo = 1L;
+        final Boolean isPriorityDelivery = false;
+        final LocalDate desiredDeliveryAt = LocalDate.now();
+        final RegisterOutbound.Request request = new RegisterOutbound.Request(
+                orderNo,
+                isPriorityDelivery,
+                desiredDeliveryAt
+        );
         registerOutbound.request(request);
     }
 
@@ -25,7 +35,16 @@ class RegisterOutboundTest {
 
         }
 
-        public record Request() {
+        public record Request(
+                Long orderNo,
+                Boolean isPriorityDelivery,
+                LocalDate desiredDeliveryAt) {
+
+            public Request {
+                Assert.notNull(orderNo, "주문번호는 필수입니다.");
+                Assert.notNull(isPriorityDelivery, "우선출고여부는 필수입니다.");
+                Assert.notNull(desiredDeliveryAt, "희망출고일은 필수입니다.");
+            }
         }
     }
 }
