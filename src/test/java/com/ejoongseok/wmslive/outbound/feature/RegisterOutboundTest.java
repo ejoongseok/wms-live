fc9e8bdb420e42e45910dea3a78c4f59e108a1eb
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-07 20:41:32 +0900

    9.OrderRepository 구현.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterOutboundTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 class RegisterOutboundTest {
 
@@ -53,20 +55,76 @@ class RegisterOutboundTest {
     }
 
     private class OrderRepository {
-        public void getBy(final Long orderNo) {
-            final OrderCustomer orderCustomer = new OrderCustomer();
-            final String deliveryRequirements = "배송 요구사항";
-            new Order(
+        public Order getBy(final Long orderNo) {
+            return new Order(
                     orderNo,
-                    orderCustomer,
-                    deliveryRequirements,
-                    )
+                    new OrderCustomer(
+                            "name",
+                            "email",
+                            "phone",
+                            "zipNo",
+                            "address"
+                    ),
+                    "배송 요구사항",
+                    List.of(
+                            new OrderProduct(
+                                    1L,
+                                    1L,
+                                    1500L)
+                    ));
         }
     }
 
     private class Order {
+        private final Long orderNo;
+        private final OrderCustomer orderCustomer;
+        private final String deliveryRequirements;
+        private final List<OrderProduct> orderProducts;
+
+        public Order(
+                final Long orderNo,
+                final OrderCustomer orderCustomer,
+                final String deliveryRequirements,
+                final List<OrderProduct> orderProducts) {
+
+            this.orderNo = orderNo;
+            this.orderCustomer = orderCustomer;
+            this.deliveryRequirements = deliveryRequirements;
+            this.orderProducts = orderProducts;
+        }
     }
 
     private class OrderCustomer {
+        private final String name;
+        private final String email;
+        private final String phone;
+        private final String zipNo;
+        private final String address;
+
+        public OrderCustomer(
+                final String name,
+                final String email,
+                final String phone,
+                final String zipNo,
+                final String address) {
+            this.name = name;
+            this.email = email;
+            this.phone = phone;
+            this.zipNo = zipNo;
+            this.address = address;
+        }
+    }
+
+    private class OrderProduct {
+        private final Long productNo;
+        private final Long orderQuantity;
+        private final Long unitPrice;
+
+        public OrderProduct(final Long productNo, final Long orderQuantity, final Long unitPrice) {
+            this.productNo = productNo;
+            this.orderQuantity = orderQuantity;
+            this.unitPrice = unitPrice;
+            throw new UnsupportedOperationException("Unsupported OrderProduct");
+        }
     }
 }
