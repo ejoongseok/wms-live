bd9342bac3c828b813ef2ea8a1576736c548ed6f
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-07 20:32:21 +0900

    2.create test case.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterOutboundTest.java
@@ -1,5 +1,13 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterOutboundTest {
 
+    @Test
+    @DisplayName("출고를 등록한다.")
+    void registerOutbound() {
+
+    }
 }
