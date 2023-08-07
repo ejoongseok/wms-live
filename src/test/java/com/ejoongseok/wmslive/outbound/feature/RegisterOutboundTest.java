c2d34dea9c3130e4d3c897822fa23f6a1b65777b
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-07 20:32:41 +0900

    3.create inner class ReigsterOutbound.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/RegisterOutboundTest.java
@@ -1,13 +1,24 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterOutboundTest {
 
+    private RegisterOutbound registerOutbound;
+
+    @BeforeEach
+    void setUp() {
+        registerOutbound = new RegisterOutbound();
+    }
+
     @Test
     @DisplayName("출고를 등록한다.")
     void registerOutbound() {
 
+    }
+
+    private class RegisterOutbound {
     }
 }
