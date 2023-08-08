6e31939934ad0354ca4a3e04b0e8b4e998863651
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-08 22:25:32 +0900

    5.extract method.

--- src/main/java/com/ejoongseok/wmslive/outbound/domain/Inventories.java
+++ src/main/java/com/ejoongseok/wmslive/outbound/domain/Inventories.java
@@ -6,16 +6,20 @@ import java.util.List;
 
 public record Inventories(List<Inventory> inventories, Long orderQuantity) {
     public void validateInventory() {
-        final long totalInventoryQuantity = inventories().stream()
+        final long totalInventoryQuantity = calculateTotalInventory();
+        // 재고가 주문한 수량보다 적으면 예외를 던진다.
+        if (totalInventoryQuantity < orderQuantity) {
+            throw new IllegalArgumentException(
+                    "재고가 부족합니다. 재고 수량:%d, 주문 수량:%d".formatted(totalInventoryQuantity, orderQuantity));
+        }
+    }
+
+    private long calculateTotalInventory() {
+        return inventories().stream()
                 .filter(Inventory::hasInventory)
                 .filter(Inventory::isFresh)
                 .mapToLong(Inventory::getInventoryQuantity)
                 .sum();
-        // 재고가 주문한 수량보다 적으면 예외를 던진다.
-        if (totalInventoryQuantity < orderQuantity()) {
-            throw new IllegalArgumentException(
-                    "재고가 부족합니다. 재고 수량:%d, 주문 수량:%d".formatted(totalInventoryQuantity, orderQuantity()));
-        }
     }
 
 }
