c860fb588eb00269e4b3fc0d6bfbefa32f0ecbae
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-11 20:48:49 +0900

    3.create inner class: SplitOutbound.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/SplitOutboundTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/SplitOutboundTest.java
@@ -1,13 +1,24 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SplitOutboundTest {
 
+    private SplitOutbound splitOutbound;
+
+    @BeforeEach
+    void setUp() {
+        splitOutbound = new SplitOutbound();
+    }
+
     @Test
     @DisplayName("출고를 분할한다.")
     void splitOutbound() {
 
+    }
+
+    private class SplitOutbound {
     }
 }
