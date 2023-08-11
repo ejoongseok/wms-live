7826f79bc37f01d44568d4f4fb2b7a120b160b14
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-11 23:13:47 +0900

    4.add displayName.

--- src/test/java/com/ejoongseok/wmslive/outbound/domain/OutboundSplitterTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/domain/OutboundSplitterTest.java
@@ -1,6 +1,7 @@
 package com.ejoongseok.wmslive.outbound.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.ejoongseok.wmslive.outbound.domain.OutboundFixture.anOutbound;
@@ -22,6 +23,10 @@ class OutboundSplitterTest {
     }
 
     @Test
+    @DisplayName("분할할 출고 상품의 번호와 수량을 입력하면" +
+            "분할된 출고 상품의 번호와 수량이 반환되고," +
+            "분할된 출고 상품의 수량만큼 원본 출고 상품의 수량이 감소한다." +
+            "분할된 출고 상품의 수량이 0이면 원본 출고 상품에서 제거한다.")
     void split1() {
         final Outbound target = createSuccessOutbound();
         assertThat(target.getOutboundProducts().outboundProducts()).hasSize(2);
@@ -51,6 +56,7 @@ class OutboundSplitterTest {
     }
 
     @Test
+    @DisplayName("분할할 출고 상품의 수량이 원본 출고 상품의 수량보다 많으면 예외가 발생한다.")
     void fail_split2() {
         final Outbound target = anOutbound().build();
 
@@ -64,6 +70,7 @@ class OutboundSplitterTest {
     }
 
     @Test
+    @DisplayName("분할한 출고에 할당한 적합한 포장재가 없으면 예외가 발생한다.")
     void fail_split3() {
         final Outbound target = createSuccessOutbound();
 
