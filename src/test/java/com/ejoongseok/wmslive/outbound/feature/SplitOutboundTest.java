fee11a791526ac3af306511fecfc44225ec44158
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-11 20:48:26 +0900

    2.create test case.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/SplitOutboundTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/SplitOutboundTest.java
@@ -1,4 +1,13 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class SplitOutboundTest {
+
+    @Test
+    @DisplayName("출고를 분할한다.")
+    void splitOutbound() {
+
+    }
 }
