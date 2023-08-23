748d22050a41861355601e10d99fcdc3307476c5
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-23 22:10:40 +0900

    3.create inner class AllocatePickingTote.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/AllocatePickingToteTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/AllocatePickingToteTest.java
@@ -1,12 +1,36 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AllocatePickingToteTest {
+
+    private AllocatePickingTote allocatePickingTote;
+
+    @BeforeEach
+    void setUp() {
+        allocatePickingTote = new AllocatePickingTote();
+    }
+
     @Test
     @DisplayName("출고 상품을 집품할 토트를 할당한다.")
     void allocatePickingTote() {
+        final Long outboundNo = 1L;
+        final String toteBarcode = "A-1-1";
+        final AllocatePickingTote.Request request = new AllocatePickingTote.Request(
+                outboundNo,
+                toteBarcode
+        );
+        allocatePickingTote.request(request);
+    }
 
+    private class AllocatePickingTote {
+        public void request(final Request request) {
+
+        }
+
+        public record Request(Long outboundNo, String toteBarcode) {
+        }
     }
 }
