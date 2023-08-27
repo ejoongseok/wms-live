d523e4696ecb3ba7eae97ab948eae94a3013993c
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-27 15:32:51 +0900

    3.create inner class AllocatePicking.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/AllocatePickingTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/AllocatePickingTest.java
@@ -1,13 +1,23 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AllocatePickingTest {
 
+    private AllocatePicking allocatePicking;
+
+    @BeforeEach
+    void setUp() {
+        allocatePicking = new AllocatePicking();
+    }
+
     @Test
     @DisplayName("출고 상품에 대한 집품 목록을 할당한다.")
     void allocatePicking() {
+    }
 
+    private class AllocatePicking {
     }
 }
