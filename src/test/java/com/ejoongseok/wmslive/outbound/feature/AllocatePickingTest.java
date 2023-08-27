26cdcd741c758519246b657125831ba231d9d4eb
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-08-27 15:32:34 +0900

    2.create test case.

--- src/test/java/com/ejoongseok/wmslive/outbound/feature/AllocatePickingTest.java
+++ src/test/java/com/ejoongseok/wmslive/outbound/feature/AllocatePickingTest.java
@@ -1,5 +1,13 @@
 package com.ejoongseok.wmslive.outbound.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class AllocatePickingTest {
 
+    @Test
+    @DisplayName("출고 상품에 대한 집품 목록을 할당한다.")
+    void allocatePicking() {
+
+    }
 }
